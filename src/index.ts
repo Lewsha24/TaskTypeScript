040ca720bc462e2200f5f520ecb0e0852e6128e0
Lewsha24 <sadkovoi0@gmail.com>
2024-04-28 22:35:30 +0700

    First commit

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,24 @@ import { MY_NUM } from "./module.js";
 
 console.warn(MY_NUM)
 
+interface PriceInput {
+    price: number;
+    discount: number;
+    isInstallment: boolean;
+    months: number;
+}
+
+const totalPrice = ({ price, discount, isInstallment, months }: PriceInput): number => {
+    const discountedPrice = price * (1 - discount / 100);
+
+    if (isInstallment && months > 0) {
+        return discountedPrice / months;
+    } else {
+        return discountedPrice;
+    }
+};
+
+const price = totalPrice({ price: 100000, discount: 25, isInstallment: true, months: 12 });
+console.log(price);
+
+// 6250
