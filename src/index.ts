a96920197f7b5bee71be6bcd4f5d0e994a649339
sadkavaya <168394036+sadkavaya@users.noreply.github.com>
2024-04-29 12:53:34 +0700

    Update index.ts
    Signed-off-by: sadkavaya <168394036+sadkavaya@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -3,25 +3,25 @@ import { MY_NUM } from "./module.js";
 console.warn(MY_NUM)
 
 // task 1
-// interface PriceInput {
-//     price: number;
-//     discount: number;
-//     isInstallment: boolean;
-//     months: number;
-// }
+interface PriceInput {
+    price: number;
+    discount: number;
+    isInstallment: boolean;
+    months: number;
+}
 
-// const totalPrice = ({ price, discount, isInstallment, months }: PriceInput): number => {
-//     const discountedPrice = price * (1 - discount / 100);
+const totalPrice = ({ price, discount, isInstallment, months }: PriceInput): number => {
+    const discountedPrice = price * (1 - discount / 100);
 
-//     if (isInstallment && months > 0) {
-//         return discountedPrice / months;
-//     } else {
-//         return discountedPrice;
-//     }
-// };
+    if (isInstallment && months > 0) {
+        return discountedPrice / months;
+    } else {
+        return discountedPrice;
+    }
+};
 
-// const price = totalPrice({ price: 100000, discount: 25, isInstallment: true, months: 12 });
-// console.log(price);
+const price = totalPrice({ price: 100000, discount: 25, isInstallment: true, months: 12 });
+console.log(price);
 
 // 6250
 
